794e5e5b515ee73dab53962f5af658313d8d358f
Arhant Jain <arhantjain2@gmail.com>
2020-12-12 19:53:49 +0530

    Improved filterBody to handle non string and non array cases

--- utils/filterBody.js
+++ utils/filterBody.js
@@ -1,5 +1,13 @@
-module.exports = (arr, obj) => {
+module.exports = (inp, obj) => {
   const filteredObj = {};
+  let arr;
+  if (!Array.isArray(inp)) {
+    if (inp === 'string') {
+      arr = inp.split(' ');
+    } else {
+      arr = {};
+    }
+  }
   arr.forEach((el) => {
     if (obj[el]) {
       filteredObj[el] = el;
